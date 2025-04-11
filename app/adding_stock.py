e0389aa8b7a184f6e0e7c7ac5b2ff7842620012f
Scr00b1e <kbolot312@gmail.com>
2025-04-11 11:06:45 +0600

    error handler

--- app/adding_stock.py
+++ app/adding_stock.py
@@ -1,78 +1,70 @@
 from aiogram import Router, F
 from aiogram.filters import StateFilter, Command
 from aiogram.fsm.context import FSMContext
-from aiogram.enums import ParseMode
 from aiogram.fsm.state import StatesGroup, State, default_state
 from aiogram.types import Message, CallbackQuery
 
 from utils.report import add_stock
-import app.keyboards as kb
 
 router = Router()
 
 class AddStock(StatesGroup):
-    choosing_stock_name  = State()
-    choosing_stock_amount  = State()
-    choosing_stock_price = State()
-
-#ADD STOCK
-@router.callback_query(F.data == 'food')
-async def add_stocks(callback: CallbackQuery):
-    report_text = add_stock()
-
-    await callback.message.answer(report_text, 
-                                  parse_mode=ParseMode.MARKDOWN, 
-                                  reply_markup=kb.report_keyboard)
-    await callback.answer()
-    await callback.message.delete()
+    stock_name  = State()
+    stock_amount  = State()
+    stock_price = State()
 
 @router.callback_query(F.data == 'add_stock')
 async def cmd_stock(callback: CallbackQuery, state: FSMContext):
     await callback.message.answer(
         text="Наименование номенклатуры:")
-    await state.set_state(AddStock.choosing_stock_name)
+    await state.set_state(AddStock.stock_name)
 
-@router.message(AddStock.choosing_stock_name, F.text)
+@router.message(AddStock.stock_name, F.text)
 async def stock_chosen(message: Message, state: FSMContext):
-    #store data
     await state.update_data(chosen_stock=message.text.lower())
-    #
     await message.answer(
-        text="Хорошо, теперь количество номенклатуры"
+        text="Хорошо, теперь количество номенклатуры (цифру):"
     )
-    await state.set_state(AddStock.choosing_stock_amount)
+    await state.set_state(AddStock.stock_amount)
 
-@router.message(StateFilter('Samsy'))
-async def stock_chosen_incorrectly(message: Message):
-    await message.answer(
-        text='I do not know this stock.\n\n'
-        "Choose the correct stock please"
-    )
+# @router.message(StateFilter('Incorrect'))
+# async def stock_chosen_incorrectly(message: Message):
+#     await message.answer(
+#         text='Неправильное значение номенклатуры.\n\n'
+#         "Введите корректные данные"
+#     )
 
-@router.message(AddStock.choosing_stock_amount, F.text)
+@router.message(AddStock.stock_amount, F.text)
 async def amount_chosen(message: Message, state: FSMContext):
-    await state.update_data(chosen_amount=message.text.lower())
-    await message.answer(
-        text="Отлично, теперь напишите цену: "
-    )
-    await state.set_state(AddStock.choosing_stock_price)
+    try:
+        await state.update_data(chosen_amount=int(message.text.lower()))
+        await message.answer(text="Отлично, теперь напишите цену (цифру): ")
+    except ValueError:
+        await message.answer(
+        text='Я не принимаю такое количество номенклатуры.\n\n'
+        'Начинайте все сначала'
+        )
+        
+    await state.set_state(AddStock.stock_price)
+    
 
-@router.message(AddStock.choosing_stock_amount)
-async def amount_chosen_incorrectly(message: Message):
-    await message.answer(
-        text='I do not know this amount of this stock.\n\n'
-        'Choose the right amount please'
-    )
-
-@router.message(AddStock.choosing_stock_price, F.text)
+@router.message(AddStock.stock_price, F.text)
 async def price_chosen(message: Message, state: FSMContext):
-    #get data
-    user_data = await state.get_data()
-    #
-    await message.answer(
-        text=f"Вы создали номенклатуру {user_data['chosen_stock']} количество: {user_data['chosen_amount']}.\n\n"
-        f"Цена: {message.text.lower()}"
-    )
+    try:
+        await state.update_data(chosen_price=int(message.text.lower()))
+        user_data = await state.get_data()
+        add_stock(user_data)
+        await message.answer(
+        text=f'Создана номенклатура с наименованием {user_data['chosen_stock']}\n'
+        f'Количество {user_data['chosen_amount']}, за цену {user_data['chosen_price']}\n\n'
+        f'Вы можете увидеть изменения в отчетах /catalog'
+        )
+    except ValueError:
+        await message.answer(
+        text='Я не принимаю такую цену номенклатуры.\n\n'
+        'Начинайте все сначала'
+        )
+
     await state.clear()
 
 #CANCEL
--- utils/report.py
+++ utils/report.py
@@ -137,37 +137,13 @@ def get_cash1c():
         return f"🚫 Ошибка при подключении к 1С:\n{str(e)}"
     
 #ADD STOCK
-def add_stock():
+def add_stock(user_data):
     url = ADDURL
 
     try:
-        response = requests.post(url, headers=headers)
+        response = requests.post(url, headers=headers, json=user_data)
         if response.status_code == 200:
-            try:
-                data = response.json()
-                if isinstance(data, dict):
-                    data = [data]
-                elif not isinstance(data, list):
-                    return f"⚠️ Ожидался список, но получено: `{type(data).__name__}`\n\n```{data}```"
-
-                df = pd.DataFrame(data)
-
-                required_columns = ["Номенклатура", "Количество", "Сумма"]
-                for col in required_columns:
-                    if col not in df.columns:
-                        df[col] = ""
-                
-                df = df[required_columns]
-
-                table = "📋 *Отчёт из 1С:*\n"
-                table += "```\n"
-                table += df.to_string(index=False)
-                table += "\n```"
-
-                return table
-
-            except Exception as e:
-                return f"❌ Ошибка при разборе JSON: {str(e)}\n\nОтвет от 1С:\n{response.text}"
+            return
         else:
             return f"❌ Ошибка от 1С: {response.status_code}\n\n{response.text}"
 
